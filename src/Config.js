107e2eed83b1f9255e93acef7591d8d8445c493f
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2023-10-19 16:32:49 +0900

    Basic actions
    - 메인시작화면
    - 게임화면
    (캐릭터 이동, 배경 캐릭터 트래킹, 적 랜덤 발현, 공격, 스코어 등)
    - 게임오버화면
    - 캐릭터/빔/배경 등 기본 스프라이트

--- src/Config.js
+++ src/Config.js
@@ -1,14 +1,14 @@
 import LoadingScene from "./scenes/LoadingScene";
-// import MainScene from "./scenes/MainScene";
-// import PlayingScene from "./scenes/PlayingScene";
-// import GameoverScene from "./scenes/GameoverScene";
+import MainScene from "./scenes/MainScene";
+import PlayingScene from "./scenes/PlayingScene";
+import GameoverScene from "./scenes/GameoverScene";
 
 const Config = {
     // 맵 크기
     width: 800,
     height: 600,
     backgroundColor: 0x000000,
-    scene: [LoadingScene],
+    scene: [LoadingScene, PlayingScene, MainScene, GameoverScene],
     pixelArt: true,
     physics: {
         default: "arcade",
